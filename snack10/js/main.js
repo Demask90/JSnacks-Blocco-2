962a673a8cf50f4b704c357f392e5b84cb35a181
Demask90 <84012055+Demask90@users.noreply.github.com>
2021-07-21 15:58:35 +0200

    fine esercizi

--- snack10/js/main.js
+++ snack10/js/main.js
@@ -5,23 +5,23 @@ var first_array = [ "topolino", "pluto", "pippo", "paperino", "17", "ananas", "m
 var second_array = ["aria", "acqua", "fuoco", "terra"];
 var push_element;
 
-
-
 if (first_array.length < second_array.length) {
-    do {
+        
+    while (first_array.length < second_array.length) {
         do {
             push_element = prompt("inserisci un elemento nel primo array");
         } while (!isNaN(push_element));
         first_array.push(push_element);  
-    } while (first_array.length < second_array.length);
+    }
 
 } else if (second_array.length < first_array.length) { 
-    do {
+    
+    while (second_array.length < first_array.length) {
         do {
             push_element = prompt("inserisci un elemento nel secondo array");
         } while (!isNaN(push_element));
         second_array.push(push_element);  
-    } while (second_array.length < first_array.length);
+    }
 }
 
 console.log(first_array);
